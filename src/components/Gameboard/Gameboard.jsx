6cdd51fc90915f0e330199503f2ed726ad3fc097
nabilk11 <khan.nabiln11@gmail.com>
2022-03-16 21:20:52 -0400

    created letter component, added to gameboard rows 5x, passed props

--- src/components/Gameboard/Gameboard.jsx
+++ src/components/Gameboard/Gameboard.jsx
@@ -1,11 +1,53 @@
 import React, { useState } from 'react'
 import { boardMatrix } from '../../WordData/Words';
+import { Letter } from '../Letter/Letter';
 
 export const Gameboard = () => {
     const [board, setBoard] = useState(boardMatrix);
   return (
-    <div>
-        Gameboard
+    <div className='board' >
+        <div className="row">
+            <Letter letterPos={0} attemptVal={0} />
+            <Letter letterPos={1} attemptVal={0} />
+            <Letter letterPos={2} attemptVal={0} />
+            <Letter letterPos={3} attemptVal={0} />
+            <Letter letterPos={4} attemptVal={0} />
+        </div>
+        <div className="row">
+            <Letter letterPos={0} attemptVal={0} />
+            <Letter letterPos={1} attemptVal={0} />
+            <Letter letterPos={2} attemptVal={0} />
+            <Letter letterPos={3} attemptVal={0} />
+            <Letter letterPos={4} attemptVal={0} />
+        </div>
+        <div className="row">
+            <Letter letterPos={0} attemptVal={0} />
+            <Letter letterPos={1} attemptVal={0} />
+            <Letter letterPos={2} attemptVal={0} />
+            <Letter letterPos={3} attemptVal={0} />
+            <Letter letterPos={4} attemptVal={0} />
+        </div>
+        <div className="row">
+            <Letter letterPos={0} attemptVal={0} />
+            <Letter letterPos={1} attemptVal={0} />
+            <Letter letterPos={2} attemptVal={0} />
+            <Letter letterPos={3} attemptVal={0} />
+            <Letter letterPos={4} attemptVal={0} />
+        </div>
+        <div className="row">
+            <Letter letterPos={0} attemptVal={0} />
+            <Letter letterPos={1} attemptVal={0} />
+            <Letter letterPos={2} attemptVal={0} />
+            <Letter letterPos={3} attemptVal={0} />
+            <Letter letterPos={4} attemptVal={0} />
+        </div>
+        <div className="row">
+            <Letter letterPos={0} attemptVal={0} />
+            <Letter letterPos={1} attemptVal={0} />
+            <Letter letterPos={2} attemptVal={0} />
+            <Letter letterPos={3} attemptVal={0} />
+            <Letter letterPos={4} attemptVal={0} />
+        </div>
 
 
     </div>
