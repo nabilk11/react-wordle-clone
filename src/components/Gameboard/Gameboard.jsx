6f1800dade478c67a5e17623092f29f4ffe197fd
nabilk11 <khan.nabiln11@gmail.com>
2022-03-16 21:12:07 -0400

    created default board matrix, with state on gameboard component

--- src/components/Gameboard/Gameboard.jsx
+++ src/components/Gameboard/Gameboard.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { boardMatrix } from '../../WordData/Words';
 
 export const Gameboard = () => {
+    const [board, setBoard] = useState(boardMatrix);
   return (
-    <div>Gameboard</div>
+    <div>
+        Gameboard
+
+
+    </div>
   )
 }
